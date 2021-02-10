04dbd266c67ceb81ffbf926ac401c088cc38ea7a
sirmcclay <mcclay.design@gmail.com>
2021-02-10 16:13:21 -0300

    update instance set and save

--- test/update_test.js
+++ test/update_test.js
@@ -8,4 +8,16 @@ describe('Updating records', () => {
 		joe = new User({ name: 'Joe' });
 		joe.save().then(() => done());
 	});
+
+	it('instance type using set and save', (done) => {
+		joe.set('name', 'Alex');
+		joe
+			.save()
+			.then(() => User.find({}))
+			.then((users) => {
+				assert(users.length === 1);
+				assert(users[0].name === 'Alex');
+				done();
+			});
+	});
 });
