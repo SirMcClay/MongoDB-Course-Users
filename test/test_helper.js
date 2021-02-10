0f71088014f585efc27579991a8280b8e824242d
sirmcclay <mcclay.design@gmail.com>
2021-02-10 09:47:57 -0300

    use Mocha before hook to execute Mongo connection and pause tests until connection is done - and uses done() to tell success to Mocha

--- test/test_helper.js
+++ test/test_helper.js
@@ -2,17 +2,21 @@ const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
-const options = {
-	useNewUrlParser: true,
-	useUnifiedTopology: true,
-};
+before((done) => {
+	const options = {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	};
 
-mongoose.connect('mongodb://localhost/users_test', options);
-mongoose.connection
-	.once('open', () => {})
-	.on('error', (error) => {
-		console.warn('Warning', error);
-	});
+	mongoose.connect('mongodb://localhost/users_test', options);
+	mongoose.connection
+		.once('open', () => {
+			done();
+		})
+		.on('error', (error) => {
+			console.warn('Warning', error);
+		});
+});
 
 beforeEach((done) => {
 	mongoose.connection.collections.users.drop(() => {
