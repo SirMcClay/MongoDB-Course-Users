6593ef96dd719bd419f66bb82061c7e6aa729da0
sirmcclay <mcclay.design@gmail.com>
2021-02-10 11:43:44 -0300

    remove joe, find him usind User model and assert the return was null

--- test/delete_test.js
+++ test/delete_test.js
@@ -1,4 +1,27 @@
 const assert = require('assert');
 const User = require('../src/user');
 
-describe('Deleting a user', () => {});
+describe('Deleting a user', () => {
+	let joe;
+
+	beforeEach((done) => {
+		joe = new User({ name: 'Joe' });
+		joe.save().then(() => done());
+	});
+
+	it('model joe instance remove', (done) => {
+		joe
+			.remove()
+			.then(() => User.findOne({ name: 'Joe' }))
+			.then((user) => {
+				assert(user === null);
+				done();
+			});
+	});
+
+	it('class User method remove', () => {});
+
+	it('class User method findAndRemove', () => {});
+
+	it('class User method findByIdAndRemove', () => {});
+});
