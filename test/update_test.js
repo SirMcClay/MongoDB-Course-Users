12cccc62d7c3c54690e3b484943d3ef6d3774b01
sirmcclay <mcclay.design@gmail.com>
2021-02-10 16:01:00 -0300

    beforeEach section

--- test/update_test.js
+++ test/update_test.js
@@ -1,4 +1,11 @@
 const assert = require('assert');
 const User = require('../src/user');
 
-describe('Updating records', () => {});
+describe('Updating records', () => {
+	let joe;
+
+	beforeEach((done) => {
+		joe = new User({ name: 'Joe' });
+		joe.save().then(() => done());
+	});
+});
