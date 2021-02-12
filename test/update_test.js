b67b8ac38bc3af8eba6e3251800b6e5395a461a1
sirmcclay <mcclay.design@gmail.com>
2021-02-12 12:21:55 -0300

    fix update postCount to likes to have a functional test about oprators, in this case inc operator

--- test/update_test.js
+++ test/update_test.js
@@ -5,7 +5,7 @@ describe('Updating records', () => {
 	let joe;
 
 	beforeEach((done) => {
-		joe = new User({ name: 'Joe', postCount: 0 });
+		joe = new User({ name: 'Joe', likes: 0 });
 		joe.save().then(() => done());
 	});
 
@@ -43,11 +43,21 @@ describe('Updating records', () => {
 		assertName(User.findByIdAndUpdate(joe._id, { name: 'Alex' }), done);
 	});
 
-	xit('a user can have their postcount incremented by 1', (done) => {
-		User.updateMany({ name: 'Joe' }, { $inc: { postCount: 1 } })
+	// Now using virtual type which cause this be broken
+	// it('a user can have their postcount incremented by 1', (done) => {
+	// 	User.updateMany({ name: 'Joe' }, { $inc: { postCount: 1 } })
+	// 		.then(() => User.findOne({ name: 'Joe' }))
+	// 		.then((user) => {
+	// 			assert(user.postCount === 1);
+	// 			done();
+	// 		});
+	// });
+
+	it('a user can have their likes incremented by 1', (done) => {
+		User.updateMany({ name: 'Joe' }, { $inc: { likes: 1 } })
 			.then(() => User.findOne({ name: 'Joe' }))
 			.then((user) => {
-				assert(user.postCount === 1);
+				assert(user.likes === 1);
 				done();
 			});
 	});
