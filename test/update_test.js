6bf26d3ad7521b50eac3d7d045e7ceb17a0a2d4a
sirmcclay <mcclay.design@gmail.com>
2021-02-12 11:59:38 -0300

    change postCount propertie on user model - mark with x update inc test for pending

--- test/update_test.js
+++ test/update_test.js
@@ -43,7 +43,7 @@ describe('Updating records', () => {
 		assertName(User.findByIdAndUpdate(joe._id, { name: 'Alex' }), done);
 	});
 
-	it('a user can have their postcount incremented by 1', (done) => {
+	xit('a user can have their postcount incremented by 1', (done) => {
 		User.updateMany({ name: 'Joe' }, { $inc: { postCount: 1 } })
 			.then(() => User.findOne({ name: 'Joe' }))
 			.then((user) => {
