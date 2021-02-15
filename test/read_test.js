0fe25ea4e292f4214889158dfbcee0f4afeae375
sirmcclay <mcclay.design@gmail.com>
2021-02-15 15:23:08 -0300

    create assertions and use one more operator - sort() - to guarantee the order of users in the returned query array

--- test/read_test.js
+++ test/read_test.js
@@ -5,7 +5,7 @@ describe('Reading users out of the database', () => {
 	let joe, maria, alex, zach;
 
 	beforeEach((done) => {
-		alex = new User({ name: 'alex' });
+		alex = new User({ name: 'Alex' });
 		joe = new User({ name: 'Joe' });
 		maria = new User({ name: 'Maria' });
 		zach = new User({ name: 'Zach' });
@@ -31,6 +31,15 @@ describe('Reading users out of the database', () => {
 
 	it('can skip and limit the result set', (done) => {
 		// -Alex- [Joe Maria] Zach
-		User.find({}).skip(1).limit(2);
+		User.find({})
+			.sort({ name: 1 })
+			.skip(1)
+			.limit(2)
+			.then((users) => {
+				assert(users.length === 2);
+				assert(users[0].name === 'Joe');
+				assert(users[1].name === 'Maria');
+				done();
+			});
 	});
 });
